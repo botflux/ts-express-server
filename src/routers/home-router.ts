519179595fefbe70a995c8c6c12a438a56be4479
Victor MENDELE <v-mendele@logitud.fr>
2019-12-12 20:49:22 +0100

    :star2: feat: add post service in home router

--- src/routers/home-router.ts
+++ src/routers/home-router.ts
@@ -1,11 +1,23 @@
 import { Router, Request, Response } from 'express'
+import PostService from '../services/post-service'
+import Post from '../models/post'
 
 export default (): Router => {
     const router: Router = Router()
 
     router
-        .get('/', (req: Request, res: Response) => res.send('hello world from home'))
-        .get('/:id', (req: Request, res: Response) => res.send('hello world from home ' + req.params.id))
+        .get('/', (req: Request, res: Response) => {
+            new PostService()
+                .findAll()
+                .then((posts: Post[]) => res.json(posts))
+                .catch(error => res.status(500).json(error))
+        })
+        .get('/:id', (req: Request, res: Response) => {
+            new PostService()
+                .find(Number.parseInt(req.params.id))
+                .then((post: Post) => res.json(post))
+                .catch(error => res.status(500).json(error))
+        })
 
     return router
 }
