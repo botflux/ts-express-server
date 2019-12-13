7e2011e997fb422bc6f9c0bc8316ef3b7fa00b83
Victor MENDELE <v-mendele@logitud.fr>
2019-12-13 12:07:01 +0100

    :star2: feat: add user router

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,27 @@
 import express, {Application} from 'express'
-import createHomeRouter from './routers/home-router'
+import fetch from 'node-fetch'
+
+import createPostRouter from './routers/post-router'
+import createUserRouter from './routers/user-router'
+
 import PostService from './services/post-service'
 import ServiceContainer from './services/service-container'
-import fetch from 'node-fetch'
+
 import {ServiceTypes} from './services/service-types'
 import {UserService} from './services/user-service'
 
 const app: Application = express()
+
+/* Services declaration */
 const serviceContainer = (new ServiceContainer())
     .addService(ServiceTypes.Fetch, () => fetch)
     .addService(ServiceTypes.PostService, (container: ServiceContainer) => new PostService(container))
     .addService(ServiceTypes.UserService, (container: ServiceContainer) => new UserService(container))
 
-app.use(createHomeRouter(serviceContainer))
+/* Routers declaration */
+app
+    .use('/posts', createPostRouter(serviceContainer))
+    .use('/users', createUserRouter(serviceContainer))
 
 const port: string | number = process.env.PORT || 3000
 
---
+++ src/routers/user-router.ts
@@ -0,0 +1,26 @@
+import {Request, Response, Router} from 'express'
+import ServiceContainer from '../services/service-container'
+import {ServiceTypes} from '../services/service-types'
+import UserServiceInterface from '../services/user-service-interface'
+import User from '../models/user'
+
+export default (serviceContainer: ServiceContainer): Router => {
+    const router: Router = Router()
+    const userService: UserServiceInterface = serviceContainer.getService(ServiceTypes.UserService)
+
+    router
+        .get('/', (req: Request, res: Response) => {
+            userService
+                .findAll()
+                .then((users: User[]) => res.json(users))
+                .catch(error => res.status(500).json(error))
+        })
+        .get('/:id', (req: Request, res: Response) => {
+            userService
+                .find(Number.parseInt(req.params.id))
+                .then((user: User) => res.json(user))
+                .catch(error => res.status(500).json(error))
+        })
+
+    return router
+}
