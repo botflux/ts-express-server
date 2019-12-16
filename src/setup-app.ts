3806b2028a576d3ff6a491872fc185f18aee3ab8
Victor MENDELE <v-mendele@logitud.fr>
2019-12-16 20:43:33 +0100

    :herb: refactor: remove controllers and decorators

--- src/setup-app.ts
+++ src/setup-app.ts
@@ -2,13 +2,9 @@ import 'reflect-metadata'
 import {Application} from 'express'
 
 import { registerServices } from './services/register-services'
-import { registerControllers } from './controllers/core/register-controllers'
 import { ServiceContainer } from './services/core/service-container'
 
 export const setupApp = (app: Application) => {
     /* Services declaration */
     const serviceContainer: ServiceContainer = registerServices(new ServiceContainer())
-
-    /* Routers declaration */
-    registerControllers(app, serviceContainer)
 }
