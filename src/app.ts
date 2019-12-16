c1ca26f84c3a95f6eade60051822906e8e466671
Victor MENDELE <v-mendele@logitud.fr>
2019-12-16 12:01:58 +0100

    :herb: refactor: export a setup app function instead of app itself in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,14 @@
 import 'reflect-metadata'
-import express, {Application} from 'express'
+import {Application} from 'express'
 
 import { registerServices } from './services/register-services'
 import { registerControllers } from './controllers/core/register-controllers'
 import { ServiceContainer } from './services/core/service-container'
 
-const app: Application = express()
+export const setupApp = (app: Application) => {
+    /* Services declaration */
+    const serviceContainer: ServiceContainer = registerServices(new ServiceContainer())
 
-/* Services declaration */
-const serviceContainer: ServiceContainer = registerServices(new ServiceContainer())
-
-/* Routers declaration */
-registerControllers(app, serviceContainer)
-
-export default app
+    /* Routers declaration */
+    registerControllers(app, serviceContainer)
+}
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
-import app from './app'
+import express from 'express'
+import {setupApp} from './app'
 
+const app = express()
 const port: string | number = process.env.PORT || 3000
+
+setupApp(app)
 
 app.listen(port, () =>
     console.log(`Application is listening on port http://localhost:${port}`))
