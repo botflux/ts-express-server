a65b644f373c5c733884d5ba0ee0e4b2667d3bd9
Victor MENDELE <v-mendele@logitud.fr>
2019-12-17 17:58:26 +0100

    :star2: feat: add user controller

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -1,11 +1,13 @@
 import {BaseController} from '../core/controllers/base-controller'
-import {ControllerInterface} from '../core/controllers/interfaces/controller-interfaces'
-import {Router, Request, Response, NextFunction} from 'express'
+import {ControllerInterface, HttpMethod} from '../core/controllers/interfaces/controller-interfaces'
+import {NextFunction, Request, Response} from 'express'
 import {ServiceTypes} from '../core/services/service-types'
 import {ApiServiceInterface} from '../services/interfaces/api-service-interface'
 import {User} from '../models/user'
+import {Controller, Route} from '../core/controllers/decorators'
 
-export class UserController extends BaseController implements ControllerInterface {
+@Controller('/users')
+export class UserController extends BaseController {
     private readonly _userService: ApiServiceInterface<User>
 
     constructor() {
@@ -13,27 +15,18 @@ export class UserController extends BaseController implements ControllerInterfac
         this._userService = this.container.getService(ServiceTypes.UserService)
     }
 
+    @Route(HttpMethod.Get, '/')
     findAll(req: Request, res: Response, next: NextFunction) {
         this._userService.findAll()
             .then((posts: User[]) => res.json(posts))
             .catch((error: Error) => next(error))
     }
 
+    @Route(HttpMethod.Get, '/:id')
     find(req: Request, res: Response, next: NextFunction) {
         this._userService.find(req.params.id)
             .then((post: User) => res.json(post))
             .catch((error: Error) => next(error))
-    }
-
-    getRouter (): Router {
-        const globalRouter = Router()
-        globalRouter.get('/', this.findAll.bind(this))
-        globalRouter.get('/:id', this.find.bind(this))
-        return globalRouter
-    }
-
-    getBasUrl() {
-        return '/users'
     }
 }
 
