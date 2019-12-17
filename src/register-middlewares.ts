845ffd22cf2c03030f5c1b65a86446e365106cb1
Victor MENDELE <v-mendele@logitud.fr>
2019-12-17 15:19:47 +0100

    :arrow_up: chore: clean up application bootstrap files

--- src/register-middlewares.ts
+++
@@ -1,8 +0,0 @@
-import {MiddlewareFactoryInterface} from './core/middlewares/interfaces/middlewares-interfaces'
-import {logRequestMiddleware} from './middlewares'
-
-export const beforeAll: MiddlewareFactoryInterface[] = [
-    logRequestMiddleware
-]
-
-export const afterAll: MiddlewareFactoryInterface[] = []
